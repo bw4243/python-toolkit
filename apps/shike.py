b4ea2e7ed7e3b98da14035c3d673cb7838697ec9
zhouzhipeng <zhipeng.zhou@dianping.com>
2016-01-11 14:16:08 +0800

    update

--- apps/shike.py
+++ apps/shike.py
@@ -139,6 +139,7 @@ def fakeDownAndOpenApp(appid, bundle_id, process_name):
         uploadProcessStatus(bundle_id, process_name)
         global succeed
         if succeed:
+            open('/data/code/python_penkie/apps/sync.txt', 'w').write('0')
             succeed = False
             break
         time.sleep(10)
