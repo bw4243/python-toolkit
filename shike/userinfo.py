c34bb6bda98408f78282ae85044713391ed8b04b
zhouzhipeng <zhipeng.zhou@dianping.com>
2016-02-20 09:21:35 +0800

    update

--- shike/userinfo.py
+++ shike/userinfo.py
@@ -76,7 +76,7 @@ def sync_user_info(user):
         User.balance: finance['can_withdrawal'],
         User.total_income: finance['history_income'],
         User.today_income: finance['today_income'],
-        User.freeze_status: finance['user_status']
+        # User.freeze_status: finance['user_status']
     })
     s.commit()
     s.close()
@@ -85,39 +85,45 @@ def sync_user_info(user):
 def bind_info(user):
     log_download_xb(user)
 
-    ss='bs=F9D145502ZJG5NZB4&cc=120'
+    ss = 'bs=%s&cc=110' % user.field1
 
-    content = 'user_id=%s&oid_md5=%s&binding=18_1&idfa=%s&idfv=%s&uid=%s&sn=no&%s&dm=iPhone8,2&sv=8.1.2&ot=-27342&mac=&rm=c2:42:d6:26:3c:6f&ri=135.24.125.100&dt=(null)&ut=451&ls=12988856311&pn=18&ver=1.19&rn=DP' \
-              % (user.user_id, user.oid_md5, user.idfa, user.idfv, user.uid,ss)
+    abc='user_id=%s&oid_md5=%s&binding=18_1&idfa=%s&idfv=%s&uid=%s&sn=no&%s&dm=iPhone8,2&sv=8.1.2&ot=-'+random_number_str(5)+'&mac=&rm=c2:42:d2:26:'+random_str(2)+':'+random_str(2)+'&ri='+random_number_str(2)+'.24.'+random_number_str(3)+'.100&dt=(null)&ut=451&ls='+random_number_str(11)+'&pn=18&ver=1.19&rn='+random_str(4)
+    content = abc % (user.user_id, user.oid_md5, user.idfa, user.idfv, user.uid, ss)
     taskrunner.json_time(str(content), url='http://xb.appshike.com/json')
 
     # content = 'user_id=%s&idfa=&bs=F5D545302WJG5NYA3&cc=340&app=1611445442,333206289,com.alipay.iphoneclient,474278250,0,143465,1,9.5.1%s1611445442,425349261,com.netease.news,474534964,0,143465,1,474%s(null),0,com.sogou.sogouinput,454115166,0,0,1,3.0.0' \
     #           % (user.user_id,  '%7C%7C', '%7C%7C')
-    content = 'user_id=' + user.user_id + '&idfa=&'+ss+'&app=1611445442,333206289,com.alipay.iphoneclient,474278250,0,143465,1,9.5.1%7C%7C1611445442,425349261,com.netease.news,474534964,0,143465,1,474%7C%7C(null),0,com.sogou.sogouinput,454115166,0,0,1,3.0.0%7C%7C(null),0,com.dianping.mp.mobile,471349340,0,0,1,4.1.0%7C%7C(null),0,com.google.chrome.ios,455139072,0,0,1,43.2357.51%7C%7C10037034958,351091731,com.dianping.dpscope,475571285,1,143465,1,8.0.0%7C%7C847712612,444934666,com.tencent.mqq,455195462,0,143465,1,5.6%7C%7C(null),0,com.dianping.apollo.crm,464783782,0,0,1,2.0.0.2%7C%7C(null),0,com.tongbu.tui.9675F56748,454500304,0,0,1,3.3.2%7C%7C(null),0,com.vstudio.PeakCamera,449226728,0,0,1,0.9.99%7C%7C1611445442,921478733,com.didapinche.taxi,475570075,0,143465,1,3.4.0%7C%7C' + user.user_id + '&ver=1.19&type=d_package'
+    content = 'user_id=' + user.user_id + '&idfa=&' + ss + '&app=1611445442,333206289,com.alipay.iphoneclient,474278250,0,143465,1,9.5.1%7C%7C1611445442,425349261,com.netease.news,474534964,0,143465,1,474%7C%7C(null),0,com.sogou.sogouinput,454115166,0,0,1,3.0.0%7C%7C(null),0,com.dianping.mp.mobile,471349340,0,0,1,4.1.0%7C%7C(null),0,com.google.chrome.ios,455139072,0,0,1,43.2357.51%7C%7C10037034958,351091731,com.dianping.dpscope,475571285,1,143465,1,8.0.0%7C%7C847712612,444934666,com.tencent.mqq,455195462,0,143465,1,5.6%7C%7C(null),0,com.dianping.apollo.crm,464783782,0,0,1,2.0.0.2%7C%7C(null),0,com.tongbu.tui.9675F56748,454500304,0,0,1,3.3.2%7C%7C(null),0,com.vstudio.PeakCamera,449226728,0,0,1,0.9.99%7C%7C1611445442,921478733,com.didapinche.taxi,475570075,0,143465,1,3.4.0%7C%7C' + user.user_id + '&ver=1.19&type=d_package'
     taskrunner.json_time(str(content), replace=1)
 
-    content = 'user_id=' + user.user_id + '&idfa=&'+ss+'&app=847712612,350962117,com.sina.weibo,453928544,0,143465,1,5.3.0%7C%7C10037034958,547166701,com.baidu.netdisk,475503388,0,143465,1,6.9.4%7C%7C(null),0,com.gad.shiliu,475550728,0,0,1,1.19%7C%7C1611445442,878534496,com.penglzh.Super12306,472454167,0,143465,1,2.0%7C%7C(null),0,com.tencent.eim,464636026,0,0,1,74557%7C%7C10037034958,414478124,com.tencent.xin,473205693,0,143465,1,6.3.9%7C%7C847712612,414245413,com.360buy.jdmobile,455462496,0,143465,1,22026%7C%7C' + user.user_id + '&ver=1.19&type=d_package'
+    content = 'user_id=' + user.user_id + '&idfa=&' + ss + '&app=847712612,350962117,com.sina.weibo,453928544,0,143465,1,5.3.0%7C%7C10037034958,547166701,com.baidu.netdisk,475503388,0,143465,1,6.9.4%7C%7C(null),0,com.gad.shiliu,475550728,0,0,1,1.19%7C%7C1611445442,878534496,com.penglzh.Super12306,472454167,0,143465,1,2.0%7C%7C(null),0,com.tencent.eim,464636026,0,0,1,74557%7C%7C10037034958,414478124,com.tencent.xin,473205693,0,143465,1,6.3.9%7C%7C847712612,414245413,com.360buy.jdmobile,455462496,0,143465,1,22026%7C%7C' + user.user_id + '&ver=1.19&type=d_package'
     taskrunner.json_time(str(content), replace=1)
 
-    content = 'user_id=%s&%s&idfa=%s&idfv=%s&uid=%s&mac=c1:41:d6:26:bc:6f&usb=28.000000&app=launchd,112685.932940,16390||nfcd,112673.934593,16388||WeChat,98017.934869,16388||eapolclient,167.936738,16644||com.apple.WebKit,62.936910,16388||com.apple.WebKit,62.936985,16388||com.apple.WebKit,61.937043,16388||com.apple.WebKit,40.937272,16388||shiliu,8.937662,16388||com.apple.WebKit,0.937784,16388||&ver=1.19&type=d_process' \
-              % (user.user_id,ss, user.idfa, user.idfv, user.uid)
+    content = 'user_id=%s&%s&idfa=%s&idfv=%s&uid=%s&mac=c2:41:d4:26:bc:6f&usb=28.000000&app=launchd,112685.932940,16390||nfcd,112673.934593,16388||WeChat,98017.934869,16388||eapolclient,167.936738,16644||com.apple.WebKit,62.936910,16388||com.apple.WebKit,62.936985,16388||com.apple.WebKit,61.937043,16388||com.apple.WebKit,40.937272,16388||shiliu,8.937662,16388||com.apple.WebKit,0.937784,16388||&ver=1.19&type=d_process' \
+              % (user.user_id, ss, user.idfa, user.idfv, user.uid)
     taskrunner.json_time(str(content))
 
 
-def add_user():
+def add_user(user_id, nick_name, cookie, oid_md5):
     user = User(
-        user_id='19308584',
-        nick_name=u'小胖子',
-        cookie='aliyungf_tc=AQAAADqAqnWjbwsABoSZtJa4rk/sKZC8; JSESSIONID=49D486B708049C300E75FDF664953EF4; OD=cO7w4o/SXBY41V9pUfeb2ZRsWs+D2bNThFzKS118hokg5ncJ6eNfkqbmbzRCFC/u',
-        oid_md5='7882C7C194D09B1FB4C54CFED4301B71',
+        user_id=user_id,
+        nick_name=nick_name,
+        cookie=cookie,
+        oid_md5=oid_md5,
         app_type=APP_TYPE_XIAO_BIN,
         idfa=gen_uuid(),
         idfv=gen_uuid(),
-        uid=random_str(40)
+        uid=random_str(40),
+        today_income=1,
+        total_income=1,
+        field1='F5'+random_str(15).upper()
     )
+
     db.add(user)
 
-    # bind_info(user)
+    user = User.get(user_id)
+    # 绑定硬件信息
+    bind_info(user)
 
 
 def log_download_xb(user):
@@ -125,7 +131,8 @@ def log_download_xb(user):
     logger.info(http_retry('http://i.appshike.com/itry/log_download_xb?oid_md5=%s' % user.oid_md5,
                            headers=get_header(user.cookie)))
 
+
 if __name__ == '__main__':
-    # add_user()
-    user=User.get('19308584')
-    bind_info(user)
+    add_user(user_id='19396391', nick_name=u'造梦', cookie='OD=OGJUnyVS5hHRz2/pZURfBLiTVGLB1ILxOZqFAQRFrEbm47WFGdfb/ySGw9pacimG', oid_md5='E51B8062A58E35C80ADF96CD9D4C7179')
+    # user = User.get('19374606')
+    # bind_info(user)
